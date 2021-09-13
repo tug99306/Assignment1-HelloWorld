217333656164d213d9e4f71749ac0d20e7aed24c
Saleem Hussein <tug99306@temple.edu>
2021-09-13 15:11:40 -0400

    Added event for button click.

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -1,11 +1,21 @@
 package edu.temple.helloworld
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myText = findViewById<TextView>(R.id.textview)
+        val myButton = findViewById<Button>(R.id.button)
+            myButton.setOnClickListener {
+                    myText.text = "You clicked the button"
+            }
     }
 }
